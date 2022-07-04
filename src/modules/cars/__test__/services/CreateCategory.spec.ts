ed09bd8d5180c425ddfcb91f9907926b8cfd2112
Bernardo Alves <behsalves@gmail.com>
2022-07-04 09:59:07 -0300

    feat: adding tests with jest

---
+++ src/modules/cars/__test__/services/CreateCategory.spec.ts
@@ -0,0 +1,17 @@
+describe("Criar categoria", () => {
+  it("Espero que 2 + 2 seja 4", () => {
+    const sum = 2 + 2;
+
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("Espero que 2 + 2 não seja 5", () => {
+    const sum = 2 + 2;
+
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
